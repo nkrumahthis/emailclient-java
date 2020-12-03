7f9f05536b122a9c8265851e1e67ea9ea46afd4a
nkrumahthis <nkrumahthis@gmail.com>
2020-12-03 00:19:20 +0000

    write envelope

--- FetchingMails.java
+++ FetchingMails.java
@@ -1,9 +1,13 @@
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Properties;
 
+import javax.mail.Address;
 import javax.mail.Folder;
 import javax.mail.Message;
+import javax.mail.Multipart;
+import javax.mail.Part;
 import javax.mail.Session;
 import javax.mail.Store;
 
@@ -35,9 +39,42 @@ public class FetchingMails {
             Message[] messages = emailFolder.getMessages();
             System.out.println("messages.length---" + messages.length);
 
+            for (int i = 0; i < messages.length; i++) {
+                Message message = messages[i];
+                System.out.println("------------");
+                writePart(message);
+
+                // TODO continue
+            }
+
             // create the folder object and open it
         } catch (Exception e) {
             // TODO: handle exception
+        }
+    }
+
+    public static void writeEnvelope(Message message) throws Exception {
+        System.out.println("This is the message envelope");
+        System.out.println("-----------------------------");
+        Address[] addresses;
+
+        // FROM
+        if ((addresses = message.getFrom()) != null) {
+            for (int j = 0; j < addresses.length; j++) {
+                System.out.println("FROM: " + addresses[j].toString());
+            }
+        }
+
+        // TO
+        if ((addresses = message.getRecipients(Message.RecipientType.TO)) != null) {
+            for (int j = 0; j < addresses.length; j++) {
+                System.out.println("TO: " + addresses[j].toString());
+            }
+        }
+
+        // SUBJECT
+        if (message.getSubject() != null) {
+            System.out.println("SUBJECT: " + message.getSubject());
         }
     }
 
